c484371e4eb0d705f60c5682c16a3c0b36840b8a
91211rathna <belderathnasree@gmail.com>
2021-09-16 14:07:07 +0530

    initial commit

---
+++ RestCrude/src/main/java/com/example/springrestboot/RestCrudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudeApplication.class, args);
+	}
+
+}
